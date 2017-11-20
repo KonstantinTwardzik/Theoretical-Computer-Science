5dc6b6c54816cf1ccd11f1bb9450ac4e8e1cdb3f
Konstantin <twardzk@hochschule-trier.de>
2017-11-20 19:00:09 +0100

    third push

--- Exercises/Exercise07/prefix_suffix.py
+++ Exercises/Exercise07/prefix_suffix.py
@@ -3,5 +3,18 @@ Created on 20.11.2017
 
 @author: Konstantin
 '''
-def prefix_set(w):
+def prefix_set(word):
+    prefixList = []
+    length = len(word) + 1
+    for i in range(length):
+        prefixList.append(word[0:i])
+    return prefixList
+        
+def suffix_set(word):
+    suffixList = []
+    length = len(word) + 1
+    for i in range(length):
+        suffixList.append(word[i:length])
+    return suffixList
+    
     
